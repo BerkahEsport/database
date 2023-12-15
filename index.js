a531248904b3a1dee0ea777cc15246d33d3a334e
BerkahEsport <berkahesport@gmail.com>
2023-12-15 15:59:02 +0700

    Update

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	Vokal: require('./utils/vokal'),
+	Base: require('./utils/base'),
+	Searchnabi: require('./utils/kisahnabi'),
+        Gempa: require('./utils/gempa'),
+		search: require('./utils/xxx'),
+		detail: require('./utils/xxx')
+}
